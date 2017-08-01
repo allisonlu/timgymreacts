d96cb4329489c72cedb4dd1f3ef4412198580c2e
allisonlu <allisonlu820@gmail.com>
2017-07-31 17:15:03 -0700

    random image generator kinda

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 /*
 TODO:
+ - generate new image every time button is clicked
  - random coordinates
- - random number generator
 */
 
 $(document).ready(function() {
-  loadImages();
+
+  var random_doge = randomImage(loadImages());
+  console.log("random dog: " + random_doge);
 
   var img = document.createElement("img");
-  img.setAttribute('src', "img/dog02.jpg");
+  img.setAttribute('src', random_doge);
+  $(img).addClass("dog-image");
 
   $('.image-button').click(function() {
-    $(this).after("hi");
-    $(this).after(img);
+    $(this).append(img);
   });
 
 });
 
+/* loadImages() pushes images from img folder into an array
+*/
 function loadImages() {
   var images = new Array();
 
-  for (var i=0; i<3; i++) {
-    images.push("img/dog0"+(i+2)+".jpg");
+  for (var i=1; i<=4; i++) {
+    images.push("img/dog0"+(i)+".jpg");
   }
 
-  console.log(images.length);
+  return images;
 }
 
+/* randomImage returns a random image (type string) from argument array
+    @param a is an array
+    @return random image (string) from array a
+*/
 function randomImage(a) {
-  var randomNum = Math.floor(Math.random()*(a.length));
-  console.log(randomNum);
+  return a[Math.floor(Math.random()*(a.length))];
 }
